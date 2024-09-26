e74c6d3fe78075134a09d0982167eda836295fe3
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-09-26 14:12:12 +0300

    smaller size (~450 mb)

--- src/main/java/lt/startupgov/boundaries/layers/Parcels.java
+++ src/main/java/lt/startupgov/boundaries/layers/Parcels.java
@@ -1,11 +1,21 @@
 package lt.startupgov.boundaries.layers;
 
 import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.FeatureMerge;
+import com.onthegomap.planetiler.ForwardingProfile;
+import com.onthegomap.planetiler.VectorTile;
+import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import lt.startupgov.boundaries.constants.Layers;
 import lt.startupgov.boundaries.constants.Source;
 
-public class Parcels implements Layer {
+import java.util.List;
+
+public class Parcels implements Layer, ForwardingProfile.LayerPostProcesser {
+    @Override
+    public String name() {
+        return Layers.PARCELS;
+    }
 
     @Override
     public void processFeature(SourceFeature sf, FeatureCollector features) {
@@ -15,7 +25,7 @@ public class Parcels implements Layer {
             features.polygon(Layers.PARCELS)
                     .setBufferPixels(4)
                     .setMinPixelSizeAtAllZooms(0)
-                    .setMinZoom(10)
+                    .setMinZoom(12)
                     .setAttr("unique_number", sf.getTag("unique_number"))
                     .setAttr("cadastral_number", sf.getTag("cadastral_number"))
                     .setAttr("status_id", sf.getTag("status_id"))
@@ -23,8 +33,17 @@ public class Parcels implements Layer {
                     .setAttr("area_ha", sf.getTag("area_ha"))
                     .setAttr("municipality_code", sf.getTag("municipality_code"))
                     .setAttr("eldership_code", sf.getTag("eldership_code"))
-                    .setAttr("updated_at", sf.getTag("updated_at"));
+                    .setAttr("updated_at", sf.getTag("updated_at"))
+                    .setPixelToleranceBelowZoom(13, 0.25);
         }
     }
 
+    @Override
+    public List<VectorTile.Feature> postProcess(int zoom, List<VectorTile.Feature> items) throws GeometryException {
+        if (zoom >= 14) {
+            return FeatureMerge.mergeMultiPolygon(items);
+        }
+
+        return FeatureMerge.mergeNearbyPolygons(items, 2, 2, 0.5, 0.5);
+    }
 }
